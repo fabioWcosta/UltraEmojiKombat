c3058adff030d02f80674ed040613364dcc720b8
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-03-01 00:36:47 -0300

    Update Lutador.java

--- Lutador.java
+++ Lutador.java
@@ -85,7 +85,9 @@ public class Lutador {
     public int getDerrotas(){
         return derrotas;
     }
-    
+    public void setDerrotas(int derrotas){
+        this.derrotas = derrotas;
+    }
 
 
 
