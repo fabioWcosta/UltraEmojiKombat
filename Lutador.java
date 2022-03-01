93863fd7bcd2f695e876f0994d4913c28f5a96e3
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-03-01 00:33:43 -0300

    Update Lutador.java

--- Lutador.java
+++ Lutador.java
@@ -73,7 +73,9 @@ public class Lutador {
     public String getCategoria(){
         return categoria;
     }
-
+    public void setCategoria(String categoria){
+        this.categoria = categoria;
+    }
 
 
 
