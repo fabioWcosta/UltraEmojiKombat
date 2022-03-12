a092a078aa01439ea2504bb591dff0ce76373e65
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-03-11 23:55:31 -0300

    Update UltraEmojiCombat.java

--- UltraEmojiCombat.java
+++ UltraEmojiCombat.java
@@ -4,7 +4,7 @@ public class UltraEmojiCombat{
                   //new Lutador(no, na, id, al, pe, vi, de, em)
         //Lutador l = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f, 11, 2, 1);
 
-        Lutador l = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f, 11, 2, 1);
+        Lutador l[] = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f, 11, 2, 1);
 
             l.status(); 
     }
