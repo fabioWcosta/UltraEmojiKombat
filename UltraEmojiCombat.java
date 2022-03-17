812cb2db07c85489ada64df3c6421693b4250478
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-03-16 23:37:59 -0300

    Update UltraEmojiCombat.java

--- UltraEmojiCombat.java
+++ UltraEmojiCombat.java
@@ -13,7 +13,7 @@ public class UltraEmojiCombat{
         l[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
         l[5] = new Lutador("Nerdart", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
         
-            l[0].status(); 
+            //l[0].status(); 
             l[2].apresentar();
             l[4].status();
     }
