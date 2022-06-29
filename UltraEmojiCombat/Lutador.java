ea55a3d4e3cf379397be53dc1bf8e03ac94af7de
fabioWcosta <71478183+fabioWcosta@users.noreply.github.com>
2022-06-28 23:59:34 -0300

    Update Lutador.java

--- UltraEmojiCombat/Lutador.java
+++ UltraEmojiCombat/Lutador.java
@@ -2,6 +2,9 @@ package UltraEmojiCombat;
 
 public class Lutador {
 
+    public Lutador(String string, String string2, int i, float f, float g, int j, int k, int l) {
+    }
+
     public void status() {
     }
 
